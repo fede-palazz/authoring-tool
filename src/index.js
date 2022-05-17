194e977694a7c03683e85a7b4215c00f9e47ef67
IlPalazz <fede.palazzi1999@gmail.com>
2022-05-17 16:00:20 +0200

    First draft of the export functionality

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import "./style.css";
 
 // Create a Modeler instance
 const viewer = new Modeler({ container: "#canvas" });
-const uploadField = document.querySelector("#uploadDiag");
-const downloadBtn = document.querySelector("#downloadDiag");
+const importDiagBtn = document.querySelector("#importDiag");
+const exportDiagBtn = document.querySelector("#exportDiag");
 
 // ********************
 // ** DIAGRAM UPLOAD **
@@ -18,20 +18,20 @@ const downloadBtn = document.querySelector("#downloadDiag");
 /**
  * File upload event listener
  */
-uploadField.addEventListener("change", () => {
+importDiagBtn.addEventListener("change", () => {
   // Null check for selected diagram
-  if (!uploadField.files) return;
+  if (!importDiagBtn.files) return;
 
   // File extension check
-  const fileName = uploadField.value;
+  const fileName = importDiagBtn.value;
   if (fileName.split(".").pop() !== "bpmn") {
     alert("Only .bpmn files can be submitted!");
     // Reset the file choice
-    uploadField.value = "";
+    importDiagBtn.value = "";
     return;
   }
   // Read the selected local diagram and display it
-  fetchAndDisplay(uploadField.files[0]);
+  fetchAndDisplay(importDiagBtn.files[0]);
 });
 
 /**
@@ -57,6 +57,41 @@ function fetchAndDisplay(file) {
   // Read the .bpmn file as plain text
   fr.readAsText(file);
 }
+
+// **********************
+// ** DOWNLOAD DIAGRAM **
+// **********************
+
+/**
+ * Save the diagram to a local .bpmn file
+ */
+async function serializeDiagram() {
+  try {
+    const { xml } = await viewer.saveXML();
+    const xmlDiag = encodeURIComponent(xml);
+    // Make the href attribute point to the diagram xlm
+    exportDiagBtn.setAttribute(
+      "href",
+      "data:application/bpmn20-xml;charset=UTF-8," + xmlDiag
+    );
+  } catch (err) {
+    console.log("Failed to serialize BPMN 2.0 xml", err);
+  }
+  // Get the diagram xml
+  // const xmlDiag = serializeDiagram();
+}
+
+exportDiagBtn.addEventListener("click", serializeDiagram);
+// () => {
+// Get the diagram xml
+//   const xmlDiag = serializeDiagram();
+
+//   // Make the href attribute point to the diagram xlm
+//   exportDiagBtn.setAttribute(
+//     "href",
+//     "data:application/bpmn20-xml;charset=UTF-8," + xmlDiag
+//   );
+// });
 
 /**
  * Display a bpmn diagram passed as XML text
