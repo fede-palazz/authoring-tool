df4b967b207f7c6fb63270f6f89e9b903f720c7f
IlPalazz <fede.palazzi1999@gmail.com>
2022-07-05 11:22:03 +0200

    Added components file

---
+++ src/components.js
@@ -0,0 +1,28 @@
+const HomeComponent = {
+  render: () => {
+    return `
+        <section>
+        <h1>Home</h1>
+        <p>Laboris ex do id duis amet exercitation veniam ut deserunt in fugiat. Nisi mollit cillum sit ut tempor duis laboris ullamco adipisicing. Minim sint dolore aute deserunt laborum veniam cillum. Aliquip occaecat duis sunt in adipisicing voluptate ad dolor dolore dolore consectetur.</p>
+      </section>
+      `;
+  },
+};
+
+const CanvasViewerComponent = {
+  render: () => {
+    return `
+        
+        `;
+  },
+};
+
+const CanvasModelerComponent = {
+  render: () => {
+    return `
+          
+          `;
+  },
+};
+
+export { HomeComponent, CanvasViewerComponent, CanvasModelerComponent };
